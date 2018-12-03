5da02c116489bfd471b6386a69339b3101b8f990
DPAUTH <dphp76@gmail.com>
2018-12-04 00:56:26 +0200

    Create usuck.h

---
+++ usuck.h
@@ -0,0 +1,11 @@
+// This is the usuck library for c,c++
+#include <iostream>
+#include <stdio.h>
+#include <unistd.h>
+
+void usuck(){
+	while (1==1){
+    printf("YOU SUCK!!!");
+        usleep(7000); // Gives the user enough time to read the message
+	}
+}
